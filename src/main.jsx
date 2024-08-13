978830c32f4201dad28ae7d44deaddaded18d472
Juani <jperezdichiazza@gmail.com>
2024-08-13 16:07:09 -0300

    main -> master in deploy.yaml

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename="/personal-portfolio/">
+  <Router basename="/personal-portfolio">
     <App />
   </Router>
   // <React.StrictMode>
